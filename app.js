2ee42fd0f35f64fea8c806a46a643d6b0c52506d
jamie.wood <w.jamie24@yahoo.co.uk>
2021-12-15 19:34:56 +0000

    new

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const cocktails = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 
 
+
+
+
 fetch(cocktails)
 .then(res => res.json())
 .then(data => {
 console.log(data.drinks[0].strDrinkThumb)
+
 document.getElementById("divhead").innerHTML = data.drinks[0].strDrink;
 document.getElementById("img").innerHTML = data.drinks[0].strDrinkThumb;
 document.getElementById("list1").innerHTML = data.drinks[0].strIngredient1
@@ -14,11 +18,10 @@ document.getElementById("list4").innerHTML = data.drinks[0].strIngredient4
 document.getElementById("list5").innerHTML = data.drinks[0].strIngredient5
 
 })
-.catch(rej => console.log(rej))
+.catch(rej => console.log(rej));
 
-fetch(cocktails)
-.then(res => res.json())
-.then(data => {
-    document.getElementById("butt").addEventListener("click")
-})
-.catch(rej => console.log(rej))   
+/*function refresh(){
+window.location.reload("Refresh")
+getElementById("butt").addEventListener("click")
+
+};*/
